eb65c1d15bfadc2306caeaa8859d5ceef665a1b9
Jemish Sonani <sonanijemish2002@gmail.com>
2023-07-24 11:05:50 +0530

    Add some plugins in CKEditor

--- Admin/ckeditor/config.js
+++ Admin/ckeditor/config.js
@@ -7,4 +7,30 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.extraPlugins = 'pbckcode';
+	config.extraPlugins = 'codesnippet';
+
+	config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert','CodeSnippet', ] },
+		'/',
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		'/',
+		'/',
+		'/',
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+	config.removeButtons = 'About';
+
 };
